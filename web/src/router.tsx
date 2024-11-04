8330ea22371f3e32e8e36c719b56952d8494f9a7
Anselmo Dias <anselmodiasyahoo@gmail.com>
2024-11-03 21:01:29 -0300

    feat: create initial config of server

--- web/src/router.tsx
+++ web/src/router.tsx
@@ -1,36 +1,23 @@
 import { createBrowserRouter } from 'react-router-dom'
 
-import { AppLayout } from './pages/_layouts/app'
 import { AuthLayout } from './pages/_layouts/auth'
-import { Dashboard } from './pages/app/dashboard'
 import { Home } from './pages/app/home'
 import { SignIn } from './pages/auth/sign-in'
 
 export const router = createBrowserRouter([
   {
-    path: '/',
-    element: <AppLayout />,
-    children: [
-      {
-        path: '/',
-        element: <Dashboard />,
-      },
-    ],
-  },
-
-  {
-    path: '/',
+    path: '/sign-in',
     element: <AuthLayout />,
     children: [
       {
-        path: '/sign-in',
+        path: '',
         element: <SignIn />,
       },
     ],
   },
 
   {
-    path: '/home',
+    path: '/',
     element: <Home />,
   },
 ])
